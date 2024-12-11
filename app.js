dec2d85c35833b51b03d8e6786455a296e4a9090
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2024-12-11 18:38:57 +0200

    Refactor Task Manager App: Set up Express server and routing. Added task controller and routes for fetching tasks. Implemented middleware for JSON parsing and defined a basic API structure.

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/hello', (req, res) => {
+  res.send('Task Manager App');
+});
+
+app.use('/api/v1/tasks', tasks);
+
+// PORT
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
